c5366f0553585eb844eb66f9e635c67d009d42c5
Sergey, the best cat ever <sergey-dzr2@yandex.ru>
2019-05-21 23:58:41 +0300

    new tests

--- src/Tests/Tester.java
+++ src/Tests/Tester.java
@@ -1,17 +1,52 @@
 package Tests;
 
-import org.junit.*;
+import main.ClojureTransformer;
+import org.junit.Test;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.*;
+import java.nio.file.FileSystems;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class Tester {
 
+    public void baseRunTest(String originalFile) {
+        String transformedFile = originalFile.substring(0, originalFile.length() - 3) + "Ans.js";
+        String srcPath = "\\src\\Tests\\";
+        String simpleJSFile = new File("").getAbsolutePath() + srcPath + originalFile;
+        String programResult = "C:\\Users\\Sergey\\Desktop\\JavaScriptChanger" + srcPath + transformedFile;
+        ClojureTransformer.transformFile(simpleJSFile, programResult);
+        String originalOutput = runFile(new File(simpleJSFile));
+        String transformedOutput = runFile(new File(programResult));
+        assertEquals("expected\n" + originalOutput + "\n finded\n" + transformedOutput, originalOutput, transformedOutput);
+    }
+
     @Test
-    public void checkSimpleJSFile(){
+    public void sampleTest(){
+        baseRunTest("Sample.js");
+    }
+    @Test
+    public void checkSimpleJSFile() {
+        baseRunTest("SimpleTest.js");
+    }
+
+    @Test
+    public void checkOneNestedFunctionFile(){
+        baseRunTest("OneNestedFunction.js");
+    }
+
+    @Test
+    public void checkManyNestedFunctions(){
+        baseRunTest("ManyNested.js");
+    }
+
+    @Test
+    public void manySameLevelFunctions(){
+        baseRunTest("ManySameLevelFunctions.js");
     }
 
 
@@ -21,8 +56,8 @@ public class Tester {
     //that both programs behavior are equals
     private File prepareFile(File file) {
         File out = new File(file.getParent(), file.getName().replace(".js", "Prepared.js"));
-        try (PrintWriter writer = new PrintWriter(new FileOutputStream(out))){
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(out))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 while (reader.ready()) {
                     String line = reader.readLine();
                     if (line.contains("return ")) {
@@ -45,18 +80,15 @@ public class Tester {
     private String runFile(File file) {
         File runedFile = prepareFile(file);
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-        Object result = null;
+        CharArrayWriter outputOfEvaluatedProgram = new CharArrayWriter();
+        engine.getContext().setWriter(outputOfEvaluatedProgram);
         try {
-            result = engine.eval(new FileReader(runedFile));
+            engine.eval(new FileReader(runedFile));
         } catch (ScriptException e) {
             System.err.println(e.getMessage());
         } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
-        if (result == null) {
-            throw new NullPointerException("Error while eval file: " + file.getName());
-        }
-        System.out.println(result + "v");
-        return result.toString();
+        return new String(outputOfEvaluatedProgram.toCharArray());
     }
 }
